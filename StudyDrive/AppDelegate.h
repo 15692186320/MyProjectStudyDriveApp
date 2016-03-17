85790f0914f4ba931a53d85ca8e1d5950e6c24be
朱元恺 <785625710@qq.com>
2016-03-18 01:04:32 +0800

    Initial Commit

---
+++ StudyDrive/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StudyDrive
+//
+//  Created by 朱元恺 on 16/3/18.
+//  Copyright © 2016年 朱元恺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StudyDrive/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StudyDrive
+//
+//  Created by 朱元恺 on 16/3/18.
+//  Copyright © 2016年 朱元恺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
